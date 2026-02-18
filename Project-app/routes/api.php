48e5fae895bd2e93cacb31403ac48f1b52507001
Ara-neko <colaste.arabelagrace@gmail.com>
2026-02-18 22:56:40 +0800

    Laravel Sanctum

--- Project-app/routes/api.php
+++ Project-app/routes/api.php
@@ -3,9 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::apiResource('users', UserController::class);
+
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
